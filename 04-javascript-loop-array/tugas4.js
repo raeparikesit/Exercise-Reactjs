9bf0f524b1ef3a7cc18eb92d531e47e50b6bd11a
rae parikesit <raeparikesit@gmail.com>
2022-10-20 22:13:38 +0700

    day 4 exercise reactjs

---
+++ 04-javascript-loop-array/tugas4.js
@@ -0,0 +1,79 @@
+console.log("\n")
+console.log("Soal 1")
+console.log("\n")
+{
+    for ( var angka = 0;angka < 10; angka++){
+        console.log(angka)
+    }
+}
+console.log("\n")
+console.log("Soal 2")
+console.log("\n")
+{
+    for(var angka = 1;angka < 10;angka+=2){
+        console.log(angka)
+    }
+}
+console.log("\n")
+console.log("Soal 3")
+console.log("\n")
+{
+    for(var angka = 0;angka < 10;angka+=2){
+        console.log(angka)
+    }
+}
+console.log("\n")
+console.log("Soal 4")
+console.log("\n")
+{
+    let array1 = [1,2,3,4,5,6]
+        console.log(array1[5])
+
+}
+console.log("\n")
+console.log("Soal 5")
+console.log("\n")
+{
+    let array2 = [5,2,4,1,3,5]
+        array2.sort()
+        console.log(array2)
+}
+console.log("\n")
+console.log("Soal 6")
+console.log("\n")
+{
+    let array3 = ["selamat", "anda", "melakukan", "perulangan", "array", "dengan", "for"]
+    for (let i=0; i < array3.length ;i++){
+        console.log(array3[i])
+
+    }
+
+}
+console.log("\n")
+console.log("Soal 7")
+console.log("\n")
+{
+    let array4 = [1, 2, 3, 4, 5, 6,7, 8, 9, 10]
+    for (let i=1; i < array4.length ;i+=2){
+        console.log(array4[i])
+    }
+
+}
+console.log("\n")
+console.log("Soal 8")
+console.log("\n")
+{
+    let kalimat= ["saya", "sangat", "senang", "belajar", "javascript"]
+    let kata=kalimat.join(" ")
+    console.log(kata)
+
+}
+console.log("\n")
+console.log("Soal 9")
+console.log("\n")
+{
+    var sayuran=[]
+    sayuran.push("kangkung","Bayam","Buncis","Kubis","Timun","Seledri","Tauge")
+    console.log(sayuran) 
+
+}
